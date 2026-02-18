2c0ec73b58c658fa46b02373c948b6d074190a35
Ankit Chaudhary <chaudharyankit2022@gmail.com>
2026-02-18 23:47:25 +0530

    Increment/Decrement Operator

--- 04 Operators and Expressions/02 Increment_Decrement Operators/src/Main.java
+++ 04 Operators and Expressions/02 Increment_Decrement Operators/src/Main.java
@@ -5,7 +5,37 @@ public class Main {
         int x = 5;
 
         x++; //6
-        ++x; //first x is 6 now it will become 7 and then assigned to x
         System.out.println(x);
+        ++x; //first x is 6 now it will become 7 and then assigned to x   PREINCREMENT
+        System.out.println(x);  //7
+
+
+        //Difference
+        int p = 5, q;
+        //q = ++p; //it will be 6 as its Pre increment value is increased first then assigned
+        q = p++; // it will be 5 as q = p =5 and value of never incremented //in loop it can be
+
+        System.out.println(q); //5
+        //amd p becomes 6
+        System.out.println(p);
+
+        for (int i = 0; i<7; i++){
+            i++;
+            p++;
+        }
+        System.out.println(p);  //p will become 5 as loop runs 6 times initial p = 5
+        /*
+        first iteration p =5
+        second iteration p = 6
+        third -> 7
+        fourth -> 8
+        fifth -> 9
+        sixth -> 10
+        end
+        p remains ->10
+         */
+
+        char c = 'A';
+        c++; //B
     }
 }
